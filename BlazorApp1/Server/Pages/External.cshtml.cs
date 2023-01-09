f83f091a33f620eb45a6561f7f46be1938b324c3
Luis Eduardo Garcia Mariano <eduardo.garcia@rd-its.com>
2023-01-09 11:45:33 -0600

    When a user registers, do not ask for confirmation and register them with the email they logged in with. Depending on the connection (Google, AZ) it registers with a different Role

--- BlazorApp1/Server/Pages/External.cshtml.cs
+++ BlazorApp1/Server/Pages/External.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Text;
 using BlazorApp1.Server.Extensions;
 using BlazorApp1.Server.Models;
 using Duende.IdentityServer.Models;
@@ -8,8 +10,11 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace BlazorApp1.Server.Pages
 {
@@ -25,15 +30,25 @@ namespace BlazorApp1.Server.Pages
         }
 
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private readonly IUserEmailStore<ApplicationUser> _emailStore;
+        
         [TempData] public string ErrorMessage { get; set; }
         public string ProviderDisplayName { get; set; }
         public string ReturnUrl { get; set; }
         [BindProperty] public InputModel Input { get; set; }
 
         public ExternalModel(
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            IUserStore<ApplicationUser> userStore)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
+            _userStore = userStore;
+            _emailStore = GetEmailStore(); 
+            
         }
 
         public IActionResult OnGetAsync(string returnurl, string schema)
@@ -83,17 +98,55 @@ namespace BlazorApp1.Server.Pages
             }
             else
             {
-                // If the user does not have an account, then ask the user to create an account.
-                ReturnUrl = returnUrl;
-                ProviderDisplayName = info.ProviderDisplayName;
-                if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
-                {
-                    Input = new InputModel
+                Input = new InputModel
                     {
                         Email = info.Principal.FindFirstValue(ClaimTypes.Email)
                     };
+                var user = CreateUser();
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                var userResult = await _userManager.CreateAsync(user);
+                if (userResult.Succeeded)
+                {
+                    var res = await _userManager.AddLoginAsync(user, info);
+                    if (res.Succeeded)
+                    {                      
+
+                        var userId = await _userManager.GetUserIdAsync(user);
+                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                        code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+                        var results = await _userManager.ConfirmEmailAsync(user, code);                        
+                        if (results.Succeeded)
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
+                            return LocalRedirect(returnUrl);
+                        }
+                        
+                    }
+                    ErrorMessage = "Error loading external login information. Internal error server";
+                    return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
                 }
-                return Page();
+                else
+                {
+                    ErrorMessage = "Error loading external login information. Internal error server";
+                    return RedirectToPage("./Login", new { ReturnUrl = returnUrl });
+                }
+            }
+        }
+
+        private ApplicationUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<ApplicationUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
+                    $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the external login page in /Areas/Identity/Pages/Account/ExternalLogin.cshtml");
             }
         }
 
@@ -114,6 +167,14 @@ namespace BlazorApp1.Server.Pages
             return LocalRedirect(Url.Content("~" + returnurl));
         }
 
+        private IUserEmailStore<ApplicationUser> GetEmailStore()
+        {
+            if (!_userManager.SupportsUserEmail)
+            {
+                throw new NotSupportedException("The default UI requires a user store with email support.");
+            }
+            return (IUserEmailStore<ApplicationUser>)_userStore;
+        }
 
     }
 }
