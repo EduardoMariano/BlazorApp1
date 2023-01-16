8d80112eb17f075cc58470ed1165bcee0d9ac744
Luis Eduardo Garcia Mariano <eduardo.garcia@rd-its.com>
2023-01-16 12:39:00 -0600

    Redirect to login when you log out

---
+++ BlazorApp1/Server/Pages/LogoutExternal.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using BlazorApp1.Server.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+
+namespace BlazorApp1.Server.Pages
+{
+    public class LogoutExternalModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;        
+
+        public LogoutExternalModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;            
+        }
+
+        public IActionResult OnGetAsync(string returnUrl)
+        {            
+            _signInManager.SignOutAsync();
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                // This needs to be a redirect so that the browser performs a new
+                // request and the identity for the user gets updated.
+                return RedirectToPage();
+            }
+        }
+    }
+}
